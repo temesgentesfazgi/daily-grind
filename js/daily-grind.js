76dccff287b7565f95dcadeb1072428979f2adda
temesgentesfazgi <natitesfazgi@gmail.com>
2024-11-20 15:19:05 -0800

    changed pumpkin to bubble tea

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,15 +1,48 @@
-myDate = new Date();
-myDay = myDate.getDay();
+/*
+
+    Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
+    One unique image, with appropriate and matching content in the alt tag.  
+    A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+    A unique color that supports the image and paragraph of content
+    The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
+
+    name - for example Bubble Tea
+    pic - for example bubble-tea.jpg
+    alt - name of coffee in alt tag
+    color - color associated with coffee
+    desc - description of coffee
+    day - day of the week
+
+
+
+*/
+
+let myDate = new Date();
+let myDay = myDate.getDay();
+let coffee = "";
+let today = "";
 
 switch(myDay){
 
-     case 0:
-         today = "Sunday";
-     break;
+    case 0:
+        today = "Sunday";
+    break;
 
-      case 2:
-         today = "Tuesday";
-     break;
+    case 2:
+        today = "Tuesday";
+
+    case 3:
+        today = "Wednesday";
+
+        coffee = {
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            alt: "A pic of a bubble tea",
+            color: "pink",
+            day: "Wednesday",
+            desc: `I like me some bubble tea!`
+        };
+    break;
 
      default:
         today = "Something went wrong";
@@ -17,3 +50,13 @@ switch(myDay){
 }
 
 alert(today);
+console.log(coffee);
+
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+function coffeeTemplate(coffee){
+    return `<p>
+    		 <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
+            <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong class="feature">
+            ${coffee.name}</strong>, ${coffee.desc}</p>`
+}
